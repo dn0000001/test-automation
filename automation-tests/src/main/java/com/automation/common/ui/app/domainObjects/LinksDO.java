9ecd7cb0f9380ed338272321604de2542efb0463
AutomatorGuy123 <AutomatorGuy123@users.noreply.github.com>
2018-11-04 13:57:45 -0500

    Example of using a data provider with limited threads

---
+++ automation-tests/src/main/java/com/automation/common/ui/app/domainObjects/LinksDO.java
@@ -0,0 +1,48 @@
+package com.automation.common.ui.app.domainObjects;
+
+import com.taf.automation.api.html.HtmlUtils;
+import com.taf.automation.ui.support.DomainObject;
+import com.taf.automation.ui.support.Utils;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import org.apache.http.HttpStatus;
+import org.jsoup.Connection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
+
+/**
+ * Class to hold all links to validated
+ */
+@XStreamAlias("links-do")
+public class LinksDO extends DomainObject {
+    private static final String USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0";
+    private List<String> links;
+
+    public List<String> getLinks() {
+        if (links == null) {
+            links = new ArrayList<>();
+        }
+
+        return links;
+    }
+
+    public static void validateUrl(String link) {
+        HtmlUtils htmlUtils = new HtmlUtils();
+        int statusCode = 1000;
+        for (int i = 0; i < 3; i++) {
+            Connection.Response response = htmlUtils.navigate(link, USER_AGENT);
+            statusCode = response.statusCode();
+            if (statusCode > HttpStatus.SC_OK) {
+                Utils.sleep(1000);
+            } else {
+                break;
+            }
+        }
+
+        assertThat(statusCode, lessThan(HttpStatus.SC_BAD_REQUEST));
+    }
+
+}
---
+++ automation-tests/src/main/java/com/automation/common/ui/app/tests/DataProviderTest.java
@@ -0,0 +1,66 @@
+package com.automation.common.ui.app.tests;
+
+import com.automation.common.ui.app.domainObjects.LinksDO;
+import com.taf.automation.ui.support.Utils;
+import com.taf.automation.ui.support.testng.TestNGBase;
+import net.jodah.failsafe.Failsafe;
+import org.apache.commons.lang3.mutable.MutableInt;
+import org.testng.ITestContext;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Features;
+import ru.yandex.qatools.allure.annotations.Severity;
+import ru.yandex.qatools.allure.annotations.Step;
+import ru.yandex.qatools.allure.annotations.Stories;
+import ru.yandex.qatools.allure.model.SeverityLevel;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Example test using a data provider
+ */
+public class DataProviderTest extends TestNGBase {
+    private List<String> links;
+
+    @BeforeTest
+    public void setup(ITestContext context) {
+        String dataSet = context.getCurrentXmlTest().getParameter(context.getName());
+        LinksDO linksDO = new LinksDO().fromResource(dataSet);
+        links = linksDO.getLinks();
+    }
+
+    @DataProvider(name = "linkData", parallel = true)
+    public Iterator<Object[]> dataProvider() {
+        List<Object[]> linkList = new ArrayList<>();
+        for (String link : links) {
+            linkList.add(new String[]{link});
+        }
+
+        return linkList.iterator();
+    }
+
+    @Features("TestNG")
+    @Stories("Data Provider")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(dataProvider = "linkData")
+    public void testLinks(String link) {
+        MutableInt attempt = new MutableInt(1);
+        Failsafe.with(Utils.getRetryPolicy()).run(() -> testLinks(attempt, link));
+    }
+
+    /**
+     * Perform validation of links from home page
+     *
+     * @param attempt - Attempt number
+     * @param link    - Link to validate
+     */
+    @Step("Validate URL - Attempt #{0}")
+    private void testLinks(MutableInt attempt, String link) {
+        attempt.increment();
+        LinksDO.validateUrl(link);
+    }
+
+}
