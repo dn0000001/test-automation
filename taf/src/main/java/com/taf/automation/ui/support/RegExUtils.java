8174ba5b4b0e6b7f2adc852e77bb268127adbbcc
AutomatorGuy123 <AutomatorGuy123@users.noreply.github.com>
2018-11-10 11:36:55 -0500

    Regular Expression Utilities

---
+++ taf/src/main/java/com/taf/automation/ui/support/RegExUtils.java
@@ -0,0 +1,32 @@
+package com.taf.automation.ui.support;
+
+/**
+ * Regular Expression Utilities
+ */
+public class RegExUtils {
+    private RegExUtils() {
+        // Prevent initialization of class as all public methods should be static
+    }
+
+    /**
+     * Treat the value as literals<BR>
+     * <B>Note: </B> This is useful if the value comes from a user
+     *
+     * @param value - Value to treat as literals
+     * @return regular expression that treat the value as literals
+     */
+    public static String treatAsLiterals(String value) {
+        return "\\Q" + value + "\\E";
+    }
+
+    /**
+     * Return the regular expression that ignores case
+     *
+     * @param regex - regular expression to ignore case
+     * @return regular expression that ignores case
+     */
+    public static String ignoreCase(String regex) {
+        return "(?i)" + regex;
+    }
+
+}
