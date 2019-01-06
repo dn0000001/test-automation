dc5456006aba9618279fcb00fe303da4461b348a
AutomatorGuy123 <AutomatorGuy123@users.noreply.github.com>
2019-01-06 13:07:47 -0500

    Add Excel data provider

---
+++ automation-tests/src/main/java/com/automation/common/ui/app/tests/Excel2013Test.java
@@ -0,0 +1,55 @@
+package com.automation.common.ui.app.tests;
+
+import com.automation.common.ui.app.domainObjects.CsvColumnMapping;
+import com.automation.common.ui.app.domainObjects.TNHC_DO;
+import com.taf.automation.ui.support.TestProperties;
+import com.taf.automation.ui.support.csv.CsvTestData;
+import com.taf.automation.ui.support.csv.CsvUtils;
+import com.taf.automation.ui.support.testng.TestNGBase;
+import org.testng.ITestContext;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Features;
+import ru.yandex.qatools.allure.annotations.Severity;
+import ru.yandex.qatools.allure.annotations.Stories;
+import ru.yandex.qatools.allure.model.SeverityLevel;
+
+import java.util.Iterator;
+
+/**
+ * Example test using a data provider that reads an excel (2013) file
+ */
+public class Excel2013Test extends TestNGBase {
+    private Iterator<Object[]> records;
+
+    @BeforeTest
+    public void setup(ITestContext injectedContext) {
+        String excelDataSet = CsvUtils.getCsvDataSetFromParameter(injectedContext, "excel");
+        String worksheet = CsvUtils.getCsvDataSetFromParameter(injectedContext, "worksheet");
+        records = CsvUtils.dataProvider(excelDataSet, worksheet, CsvColumnMapping.RUN.getColumnName()).iterator();
+    }
+
+    @DataProvider(name = "ExcelData", parallel = true)
+    public Iterator<Object[]> dataProvider() {
+        return records;
+    }
+
+    @Features("TestNG")
+    @Stories("Excel Data Provider")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(dataProvider = "ExcelData")
+    public void runTestPermutation(CsvTestData csvTestData, ITestContext injectedContext) {
+        CsvUtils.testRunner(this, injectedContext, () -> runWrapper(csvTestData));
+    }
+
+    private void runWrapper(CsvTestData csvTestData) {
+        TNHC_DO tnhc = new TNHC_DO(getContext());
+        CsvUtils.initializeDataAndAttach(tnhc, csvTestData);
+
+        getContext().getDriver().get(TestProperties.getInstance().getURL());
+        tnhc.getLanding().setPlayer();
+        tnhc.getLanding().setTeam();
+    }
+
+}
