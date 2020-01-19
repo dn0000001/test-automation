914c8de7450ce6a8a8483fc67a78edf040cd8c09
AutomatorGuy123 <AutomatorGuy123@users.noreply.github.com>
2020-01-19 14:40:10 -0500

    Re-factor Expected Conditions Matcher Test & Lambda Expression Matcher Test to use a common page object

---
+++ automation-tests/src/main/java/com/automation/common/ui/app/pageObjects/AddRemoveElementsPage.java
@@ -0,0 +1,96 @@
+package com.automation.common.ui.app.pageObjects;
+
+import com.taf.automation.ui.support.ExpectedConditionsUtil;
+import com.taf.automation.ui.support.Helper;
+import com.taf.automation.ui.support.PageObjectV2;
+import com.taf.automation.ui.support.TestContext;
+import com.taf.automation.ui.support.Utils;
+import com.taf.automation.ui.support.conditional.Criteria;
+import com.taf.automation.ui.support.conditional.CriteriaMaker;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import ru.yandex.qatools.allure.annotations.Step;
+import ui.auto.core.components.WebComponent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Add/Remove Elements Page at
+ * <a href="https://the-internet.herokuapp.com/add_remove_elements/">https://the-internet.herokuapp.com/add_remove_elements/</a>
+ */
+public class AddRemoveElementsPage extends PageObjectV2 {
+    @XStreamOmitField
+    @FindBy(css = ".example > button")
+    private WebComponent addElementButton;
+
+    @FindBy(css = ".added-manually")
+    private WebComponent deleteButton;
+
+    public AddRemoveElementsPage() {
+        super();
+    }
+
+    public AddRemoveElementsPage(TestContext context) {
+        super(context);
+    }
+
+    @Step("Click Add Element")
+    public void clickAddElement() {
+        click(addElementButton);
+    }
+
+    @Step("Click Delete Button")
+    public void clickDeleteButton() {
+        WebElement element = click(deleteButton);
+        Utils.getWebDriverWait().until(ExpectedConditions.invisibilityOf(element));
+    }
+
+    @Step("Wait For Add Element Using Lambda Expression")
+    public void waitForAddElementUsingLambdaExpression() {
+        List<Criteria> criteria = new ArrayList<>();
+        criteria.add(CriteriaMaker.forLambdaExpression(this::isAddElementForLambdaExpression));
+        Helper.assertThat(getContext().getDriver(), criteria);
+    }
+
+    private boolean isAddElementForLambdaExpression() {
+        return getDriver().findElement(addElementButton.getLocator()).isDisplayed();
+    }
+
+    @Step("Wait For Delete Button Using Lambda Expression")
+    public void waitForDeleteButtonUsingLambdaExpression() {
+        List<Criteria> criteria = new ArrayList<>();
+        criteria.add(CriteriaMaker.forLambdaExpression(this::isDeleteButtonForLambdaExpression));
+        Helper.assertThat(getContext().getDriver(), criteria);
+    }
+
+    private boolean isDeleteButtonForLambdaExpression() {
+        return getDriver().findElement(deleteButton.getLocator()).isDisplayed();
+    }
+
+    @Step("Wait For Add Element Using Expected Condition")
+    public void waitForAddElementUsingExpectedCondition() {
+        List<Criteria> criteria = new ArrayList<>();
+        criteria.add(CriteriaMaker.forExpectedConditions(isAddElementForExpectedCondition()));
+        Helper.assertThat(getContext().getDriver(), criteria);
+    }
+
+    private ExpectedCondition<WebElement> isAddElementForExpectedCondition() {
+        return ExpectedConditions.visibilityOfElementLocated(addElementButton.getLocator());
+    }
+
+    @Step("Wait For Delete Button Using Expected Condition")
+    public void waitForDeleteButtonUsingExpectedCondition() {
+        List<Criteria> criteria = new ArrayList<>();
+        criteria.add(CriteriaMaker.forExpectedConditions(isDeleteButtonForExpectedCondition()));
+        Helper.assertThat(getContext().getDriver(), criteria);
+    }
+
+    private ExpectedCondition<WebElement> isDeleteButtonForExpectedCondition() {
+        return ExpectedConditionsUtil.ready(deleteButton.getLocator());
+    }
+
+}
--- automation-tests/src/main/java/com/automation/common/ui/app/tests/ExpectedConditionsMatcherTest.java
+++ automation-tests/src/main/java/com/automation/common/ui/app/tests/ExpectedConditionsMatcherTest.java
@@ -1,14 +1,7 @@
 package com.automation.common.ui.app.tests;
 
-import com.taf.automation.ui.support.ExpectedConditionsUtil;
-import com.taf.automation.ui.support.Helper;
-import com.taf.automation.ui.support.conditional.Criteria;
-import com.taf.automation.ui.support.conditional.CriteriaMaker;
+import com.automation.common.ui.app.pageObjects.AddRemoveElementsPage;
 import com.taf.automation.ui.support.testng.TestNGBase;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -16,9 +9,6 @@ import ru.yandex.qatools.allure.annotations.Features;
 import ru.yandex.qatools.allure.annotations.Severity;
 import ru.yandex.qatools.allure.annotations.Stories;
 import ru.yandex.qatools.allure.model.SeverityLevel;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Testing Conditional - ExpectedConditionsMatch
@@ -31,33 +21,12 @@ public class ExpectedConditionsMatcherTest extends TestNGBase {
     @Test
     public void performTest(@Optional("https://the-internet.herokuapp.com/add_remove_elements/") String url) {
         getContext().getDriver().get(url);
-        Helper.assertThat(getContext().getDriver(), getAddElementCriteria());
-        clickAddElement();
-        Helper.assertThat(getContext().getDriver(), getDeleteButtonCriteria());
-    }
-
-    private List<Criteria> getAddElementCriteria() {
-        List<Criteria> criteria = new ArrayList<>();
-        criteria.add(CriteriaMaker.forExpectedConditions(isAddElement()));
-        return criteria;
-    }
-
-    private List<Criteria> getDeleteButtonCriteria() {
-        List<Criteria> criteria = new ArrayList<>();
-        criteria.add(CriteriaMaker.forExpectedConditions(isDeleteButton()));
-        return criteria;
-    }
-
-    private ExpectedCondition<WebElement> isAddElement() {
-        return ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".example > button"));
-    }
-
-    private ExpectedCondition<WebElement> isDeleteButton() {
-        return ExpectedConditionsUtil.ready(By.cssSelector(".added-manually"));
-    }
-
-    private void clickAddElement() {
-        getContext().getDriver().findElement(By.cssSelector(".example > button")).click();
+        AddRemoveElementsPage addRemoveElementsPage = new AddRemoveElementsPage();
+        addRemoveElementsPage.initPage(getContext());
+        addRemoveElementsPage.waitForAddElementUsingExpectedCondition();
+        addRemoveElementsPage.clickAddElement();
+        addRemoveElementsPage.waitForDeleteButtonUsingExpectedCondition();
+        addRemoveElementsPage.clickDeleteButton();
     }
 
 }
