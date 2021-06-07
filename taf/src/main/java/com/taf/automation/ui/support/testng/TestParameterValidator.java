5a3055a8a33d18356b10d325a8ed85a590a0d0f7
AutomatorGuy123 <AutomatorGuy123@users.noreply.github.com>
2021-06-06 21:12:12 -0400

    Upgrade to TestNG 7.4.0

--- taf/src/main/java/com/taf/automation/ui/support/testng/TestParameterValidator.java
+++ taf/src/main/java/com/taf/automation/ui/support/testng/TestParameterValidator.java
@@ -3,26 +3,29 @@ package com.taf.automation.ui.support.testng;
 import org.testng.IInvokedMethod;
 import org.testng.IInvokedMethodListener;
 import org.testng.ITestResult;
+import org.testng.TestNGException;
 import org.testng.annotations.Parameters;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 public class TestParameterValidator implements IInvokedMethodListener {
-    private static final String MSG = "Parameter %s for method %s in class %s was not found in test suite file!";
-
     @SuppressWarnings("java:S112")
     @Override
     public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
         Method testMethod = method.getTestMethod().getConstructorOrMethod().getMethod();
         Annotation paramAnnotation = testMethod.getAnnotation(Parameters.class);
-        if (paramAnnotation != null && testResult.getTestContext() != null) {
+        if (paramAnnotation != null && testResult.getTestContext() != null && method.getTestMethod().isTest()) {
             String[] params = ((Parameters) paramAnnotation).value();
             for (String param : params) {
                 String value = testResult.getTestContext().getCurrentXmlTest().getParameter(param);
                 if (value == null) {
-                    String msg = String.format(MSG, param, testMethod.getName(), testMethod.getDeclaringClass().getName());
-                    throw new RuntimeException(msg);
+                    String xmlSuiteFileName = testResult.getTestContext().getSuite().getXmlSuite().getFileName();
+                    String xmlSuiteInfo = xmlSuiteFileName != null ? "in " + xmlSuiteFileName : "";
+                    throw new TestNGException("Parameter '" + param + "' is required by method " + testMethod.getName()
+                            + " in class " + testMethod.getDeclaringClass().getName()
+                            + " but has not been marked @Optional or defined\n"
+                            + xmlSuiteInfo);
                 }
             }
         }
